897266ded3b8563f08962a298e6b8d008125a2f6
Preeti-2023 <89685482+Preeti-2023@users.noreply.github.com>
2023-10-26 11:25:19 +0000

    conversion

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,8 +2,12 @@ const accountId=1
 let accountEmail= "prityraj609@gmail.com"
 var accountPassword= "12345"
 accountCity ="Delhi"
-
+let accountState;
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
 
 //accountId=2
 console.log(accountId);
-console.table([accountEmail,accountPassword,accountCity, accountId])
+console.table([accountEmail,accountPassword,accountCity, accountId,accountState])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,54 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello")
+
+let name="preeti"
+let age=19
+let isLoggedIn=false
+
+
+//number =>2to power 53
+//bigint
+//string=>" "
+//boolean=> trye/false
+//null=> standalone value
+// undefined=> 
+//symbol=> unique
+
+
+//object
+console.log(typeof undefined);// undefined
+console.log(typeof null) ; // object
+
+
+
+//Conversion
+
+let score =true
+console.log(typeof score);
+console.log(typeof (score));
+let valueIntNumber= Number(score)
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+/*
+ "33"=> 33
+ "33abc"=> nan not a number nd type number 
+ true=>1
+
+*/
+
+let isLoggedIn1="p"
+let booleanIsLoggedIn1= Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1)
+/*
+
+1=> true; 0=> false
+""=> false
+"hitesh"=> true
+*/
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
