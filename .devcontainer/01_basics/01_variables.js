6ee5f37f1af1cb009bc46d8c201a8c86f99d65da
Preeti-2023 <89685482+Preeti-2023@users.noreply.github.com>
2023-10-26 05:47:45 +0000

    commit

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=1
+let accountEmail= "prityraj609@gmail.com"
+var accountPassword= "12345"
+accountCity ="Delhi"
+
+
+//accountId=2
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity, accountId])
